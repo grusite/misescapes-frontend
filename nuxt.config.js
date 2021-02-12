1969819a18bd8d65495faf9fd0f7f95e751f0c7f
Jorge <grusite@gmail.com>
2021-02-13 00:35:21 +0100

    Added components in pages and kept the old components folder for global components

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,7 @@ export default {
   plugins: ['~/plugins/maps.client', '~/plugins/dataApi', '~/plugins/apiDto', '~/plugins/toast'],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
-  components: true,
+  components: ['~/components', { path: '~/pages', pattern: '*/components/**' }],
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
